c379caa5d9351d29286ad93dac0050493dce22fe
sk <remya.ul@gmail.com>
2022-08-04 16:46:39 +0530

    First commit

---
+++ RemyaJavaApp/src/Demo.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author remya
+ *
+ */
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world......");
+	}
+
+}
